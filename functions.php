5fd88473645da01b1d0eab4bfb7995fd284bd745
pogibryan <bryanrsebastian@gmail.com>
2017-10-10 10:14:47 +0800

    Add rewrite-rules guide and update style.css comment.

--- functions.php
+++ functions.php
@@ -9,8 +9,9 @@ include_once( 'functions/theme-support.php' );
 include_once( 'functions/menus.php' );
 include_once( 'functions/body-class.php' );
 include_once( 'functions/wp-title.php' );
-// include_once( 'functions/options.php' );
-// include_once( 'functions/tinymce.php' );
+// include_once( 'functions/rewrite-rules.php' );
+// include_once( 'functions/options.php' ); /* Required ACF Plugin */
+// include_once( 'functions/tinymce.php' ); /* Reqyured Advanced TinyMCE Plugin */
 // include_once( 'functions/backend.php' );
 // include_once( 'functions/page-cat.php' );
 // include_once( 'functions/term-settings.php' );
